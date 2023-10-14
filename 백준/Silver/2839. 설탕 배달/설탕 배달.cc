a69b6a27a700d58567c6460e973a6d395ff77c76
LeeeJooo <56886385+LeeeJooo@users.noreply.github.com>
2023-10-14 12:10:41 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,4 +1,4 @@
-// TOP-DOWN
+// BOTTOM-UP
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -6,36 +6,35 @@ using namespace std;
 int N;  // 최종 무게
 int weights[5001];  // idx:배달한무게  value:봉지개수
 
-int Memoization(int now);
-
 int main() {
-    cin >> N;   // 최종 무게 입력
+    cin >> N;  // 최종 무게 입력
 
-    // 3kg, 5kg 값
+    // 3kg, 5kg 초기화
     weights[3] = 1;
     weights[5] = 1;
 
-    // 풀이
-    weights[N] = Memoization(N);
+    int n =6;
+    for (n=6; n<=N; n++) {
+        // 5의 배수인 경우
+        if (n%5==0) {
+            weights[n] = weights[n-5]+1;
+            continue;
+        }
 
-    // 출력
+        // 3의 배수인 경우
+        if (n%3==0) {
+            weights[n] = weights[n-3]+1;
+            continue;
+        }
+
+        // 그 외
+        if (weights[n-3] && weights[n-5]) weights[n] = min(weights[n-3], weights[n-5])+1;
+        else if (weights[n-3]) weights[n] = weights[n-3]+1;
+        else if (weights[n-5]) weights[n] = weights[n-5]+1;
+    }
+
     if (!weights[N]) cout << -1;
     else cout << weights[N];
 
     return 0;
-}
-
-int Memoization(int now) {
-    // 이미 구한 경우 >> 그 값을 반환
-    if (weights[now]) return weights[now];
-
-    // (now-3) / (now-5)kg을 옮기는 데 필요한 봉지 수, 옮길 수 없는 경우 0 이하
-    int added_3 = 0, added_5 = 0;
-    if (now-3>=3) added_3 = Memoization(now-3);
-    if (now-5>=3) added_5 = Memoization(now-5);
-
-    if (added_3>0 && added_5>0) return weights[now] = min(added_3, added_5) + 1;
-    if (added_3>0) return weights[now] = added_3+1;
-    if (added_5>0) return weights[now] = added_5+1;
-    return -1;
 }
