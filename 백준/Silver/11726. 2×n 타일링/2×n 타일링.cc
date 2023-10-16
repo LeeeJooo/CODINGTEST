432123d8e2fc184c3f35996253d861edc621f51f
LeeeJooo <56886385+LeeeJooo@users.noreply.github.com>
2023-10-16 21:33:38 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,29 +1,23 @@
-// TOP-DOWN
+// BOTTOM-UP
 #include <iostream>
 using namespace std;
-
 int N;  // 가로 길이
 int tiling[1001];   // idx:세로길이  value:타일개수
 
-int DP(int n);
-
 int main() {
     cin >> N;   // 가로 길이 입력
-    
+
     // 2x1, 2x2 초기화
     tiling[1] = 1;
     tiling[2] = 2;
 
     // DP
-    tiling[N] = DP(N);
+    for (int i=3; i<=N; i++) {
+        tiling[i]=(tiling[i-1]+tiling[i-2])%10007;
+    }
 
     // 출력
-    cout << tiling[N] % 10007;
+    cout << tiling[N];
 
     return 0;
-}
-
-int DP(int n) {
-    if (tiling[n]) return tiling[n];
-    return tiling[n] = (DP(n-1) + DP(n-2))%10007;
 }
