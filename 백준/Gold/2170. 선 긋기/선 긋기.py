3de26197fd247ba5edaa796f4f07d69d25e1c5cb
LeeeJooo <56886385+LeeeJooo@users.noreply.github.com>
2024-04-12 21:57:05 +0900

    [Gold V] Title: 선 긋기, Time: 2520 ms, Memory: 269768 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -0,0 +1,22 @@
+N = int(input())    # 선을 그은 횟수 (1이상 1,000,000이하)
+nums = []
+for _ in range(N):
+    nums.append(list(map(int, input().split())))    # (시작점, 끝점)
+
+# 시작점을 기준으로 정렬
+nums.sort(key=lambda x:x[0])
+
+start, end = nums[0]
+ans = 0
+for i in range(1,N):
+    # 기준 선과 겹치거나 이어지는 선 찾기
+    if start<=nums[i][0]<=end :
+        end = max(end,nums[i][1])
+        continue
+
+    # 선 길이 구하기
+    ans += end-start
+    start,end = nums[i]  # 기준 시작점, 끝점 update
+
+ans += end-start
+print(ans)
