6f07a2259080c39a3009b992d6ad83ecb3e6048c
LeeeJooo <56886385+LeeeJooo@users.noreply.github.com>
2024-04-24 03:00:45 +0900

    [Silver IV] Title: 정수 제곱근, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -1,16 +1,3 @@
 n = int(input())
-
-def binary_search(s,e, n):
-    if s>=e :
-        print(s)
-        exit(0)
-    mid = (s+e)//2
-    if mid**2 == n :
-        print(mid)
-        exit(0)
-    if mid**2 > n :
-        binary_search(s, mid,n)
-    else:
-        binary_search(mid+1, e,n)
-
-binary_search(0,n,n)
+m = int(n**.5)
+print(m + (m*m<n))
